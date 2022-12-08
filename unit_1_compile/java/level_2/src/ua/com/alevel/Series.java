399bbfeffe0e001e4bb8dc41f473fa0522784477
User <@ValentinaHolovko>
2022-12-08 10:53:52 +0200

    add gcc compilation

--- unit_1_compile/java/level_2/src/ua/com/alevel/Series.java
+++ unit_1_compile/java/level_2/src/ua/com/alevel/Series.java
@@ -1,10 +1,6 @@
 package ua.com.alevel;
-
-import ua.com.alevel.test.Master;
-
 class Series {
     public static void main(String[] args) {
-        Master msg = new Master();
-        msg.showMaster();
+        System.out.println("Created first series");
     }
 }
