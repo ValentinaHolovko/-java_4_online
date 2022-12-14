97afb040416fa1a74dbc528c3b3f15545711f32f
User <@ValentinaHolovko>
2022-12-14 18:07:17 +0200

    add Group

---
+++ unit_2_about_class/src/main/java/ua/com/alevel/Group.java
@@ -0,0 +1,16 @@
+package ua.com.alevel;
+
+public class Group {
+
+    String name = "T-41";
+    boolean isStupid = false;
+
+    void sendMessage(String message, int count) {
+        if (count == 6) {
+            System.out.println("message = " + message + " is done");
+
+        }
+
+    }
+
+}
--- unit_2_about_class/src/main/java/ua/com/alevel/Main.java
+++ unit_2_about_class/src/main/java/ua/com/alevel/Main.java
@@ -1,18 +1,32 @@
 package ua.com.alevel;
-class EggVoice extends Thread
-{
-    @Override
-    public void run()
-    {
-        for(int i = 0; i < 5; i++)
-        {
-            try{
-                sleep(6);
-            }catch(InterruptedException e){}
 
-            System.out.println("яйцо!");
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 30;
+        Random random = new Random();
+        Group[] hw = new Group[random.nextInt(count)];
+        System.out.println("hw = " + hw.length);
+
+        for (int i = 0; i < hw.length; i++) {
+            hw[i] = new Group();
+            hw[i].name = String.valueOf(i);
+            random = new Random();
+            int result = random.nextInt(3);
+            if (result == 0) {
+                hw[i].isStupid = true;
+            } else {
+                hw[i].isStupid = false;
+            }
         }
 
+        for (int i = 0; i < hw.length; i++) {
+            System.out.println(
+                    "hw = " + hw[i].name + " " + hw[i].isStupid
+            );
+
+        }
     }
 }
-
