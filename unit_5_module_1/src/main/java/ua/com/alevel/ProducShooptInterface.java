9845852ebd773efebae1b031a287c15355ccf87e
User <@ValentinaHolovko>
2023-01-06 14:08:42 +0200

    unit 5

--- unit_5_module_1/src/main/java/ua/com/alevel/ProducShooptInterface.java
+++ unit_5_module_1/src/main/java/ua/com/alevel/ProducShooptInterface.java
@@ -26,35 +26,64 @@ public class ProducShooptInterface {
         System.out.println();
         System.out.println("1. If you would like to add a product name, please enter - 1");
         System.out.println("2. If you would like to add a shoop name, please enter - 2");
-        System.out.println("3. If you want find product, please enter name - 3");
-        System.out.println("4. If you want find shoop, please enter name - 4");
-        System.out.println("5.If You want to update a product, please enter 5");
-        System.out.println("6. If You want to update a shoop, please enter 6");
-        System.out.println("7. If You want to attach a product to the car, please enter 7");
-        System.out.println("8. If You want to delete Product to Shoop 8");
-        System.out.println("9. If you want delete product, please enter name - 9");
-        System.out.println("10. If you want delete shoop, please enter name - 10");
+        System.out.println("3. If you want find product, please enter id - 3");
+        System.out.println("4. If you want find shoop, please enter id - 4");
+        System.out.println("5. If You want to update a product, please enter - 5");
+        System.out.println("6. If You want to update a shoop, please enter - 6");
+        System.out.println("7. If You want to attach a product to the shoop, please enter - 7");
+        System.out.println("8. If You want to delete product to shoop - 8");
+        System.out.println("9. If you want delete product, please enter id - 9");
+        System.out.println("10. If you want delete shoop, please enter id - 10");
         System.out.println("11. If you want find all product and product, please enter - 11");
         System.out.println("12. If you want find all product and shoop, please enter - 12");
-        System.out.println("13. If you want close application, please enter - 13");
+        System.out.println("13. Press to exit the offer - 13");
         System.out.println();
     }
 
     private static void crud(BufferedReader reader, String select) throws IOException {
         switch (select) {
-            case "1" : createProduct(reader); break;
-            case "2" : createShoop(reader); break;
-            case "3" : findProduct(reader); break;
-            case "4" : findShoop(reader); break;
-            case "5" : updateProduct(reader);break;
-            case "6" : updateShoop(reader);break;
-            case "7" : attachProductToShoop(reader);break;
-            case "8" : deleteProductToShoop(reader);break;
-            case "9" : deleteProduct(reader); break;
-            case "10" : deleteShoop(reader); break;
-            case "11" : findAllProduct(); break;
-            case "12" : findAllShoop(); break;
-            case "13" : exit(); break;
+            case "1":
+                createProduct(reader);
+                break;
+            case "2":
+                createShoop(reader);
+                break;
+            case "3":
+                findProduct(reader);
+                break;
+            case "4":
+                findShoop(reader);
+                break;
+            case "5":
+                updateProduct(reader);
+                break;
+            case "6":
+                updateShoop(reader);
+                break;
+            case "7":
+                attachShoopToProduct(reader);
+                break;
+            case "8":
+                attachProductToShoop(reader);
+                break;
+            case "9":
+                deleteProductToShoop(reader);
+                break;
+            case "10":
+                deleteProduct(reader);
+                break;
+            case "11":
+                deleteShoop(reader);
+                break;
+            case "12":
+                findAllProduct();
+                break;
+            case "13":
+                findAllShoop();
+                break;
+            case "14":
+                exit();
+                break;
             default:
                 throw new IllegalStateException("Unexpected value: " + select);
         }
@@ -75,16 +104,17 @@ public class ProducShooptInterface {
         product.setCountry(country);
         DbStorage.addProduct(product);
     }
+
     private static void createShoop(BufferedReader reader) throws IOException {
-      System.out.println("Create shoop");
-      System.out.println("Please enter name");
-      String name = reader.readLine();
-      System.out.println("Please enter country");
-      String country = reader.readLine();
-      Shoop shoop = new Shoop();
-      shoop.setShoopName(name);
-      shoop.setCountry(country);
-      DbStorage.addShoop(shoop);
+        System.out.println("Create shoop");
+        System.out.println("Please enter name");
+        String name = reader.readLine();
+        System.out.println("Please enter country");
+        String country = reader.readLine();
+        Shoop shoop = new Shoop();
+        shoop.setShoopName(name);
+        shoop.setCountry(country);
+        DbStorage.addShoop(shoop);
     }
 
     private static void findProduct(BufferedReader reader) throws IOException {
@@ -93,89 +123,111 @@ public class ProducShooptInterface {
         Product product = DbStorage.getProduct(id);
         System.out.println("product = " + product);
     }
-        private static void findShoop(BufferedReader reader) throws IOException {
-            System.out.println("Find shoop by id");
-            String id = reader.readLine();
-            Shoop shoop = DbStorage.getShoop(id);
-            System.out.println("shoop = " + shoop);
-        }
-            private static void updateProduct(BufferedReader reader) throws IOException {
-                System.out.println("Let's update product's information. To start, please enter product's ID:");
-                String productId = reader.readLine();
-                Product product = DbStorage.getProduct(productId);
-                if(product == null){
-                    System.out.println("This product does not exist.");
-                } else {
-                    System.out.println("Please enter product's new name:");
-                    String productName = reader.readLine();
-                    product.setProductName(productName);
-                    System.out.println("Please enter product's price:");
-                    String price = reader.readLine();
-                    product.setPrice(price);
-                    System.out.println("Please enter product's country:");
-                    String country = reader.readLine();
-                    product.setCountry(country);
-                    System.out.println("Thank you, the product's information is updated.");
-                }
-            }
-            private static void updateShoop(BufferedReader reader) throws IOException {
-                System.out.println("Let's update shoop's information. To start, please enter shoop's ID:");
-                String shoopId = reader.readLine();
-                Shoop shoop = DbStorage.getShoop(shoopId);
-                if(shoop == null){
-                    System.out.println("This shoop does not exist.");
-                } else {
-                    System.out.println("Please enter shoop's name:");
-                    String shoopName = reader.readLine();
-                    shoop.setShoopName(shoopName);
-                    System.out.println("Please enter product's country:");
-                    String country = reader.readLine();
-                    shoop.setCountry(country);
-                    System.out.println("Thank you, the shoop's information is updated.");
-                }
-            }
-    private static void attachProductToShoop(BufferedReader reader) throws IOException {
-        System.out.println("Attach product to shoop");
-        System.out.println("Please enter shoop id");
-        String productId = reader.readLine();
-        System.out.println("Please enter product id");
-        String shoopId = reader.readLine();
-        DbStorage.addProductToShoop(shoopId, productId);
-        }
-    private static void deleteProductToShoop(BufferedReader reader) throws IOException {
-        System.out.println("Delete product to shoop");
-        System.out.println("Please enter shoop id");
-        String shoopId = reader.readLine();
-        System.out.println("Please enter product id");
-        String productId = reader.readLine();
-        DbStorage.deleteProductToShoop(productId, shoopId);
+
+    private static void findShoop(BufferedReader reader) throws IOException {
+        System.out.println("Find shoop by id");
+        String id = reader.readLine();
+        Shoop shoop = DbStorage.getShoop(id);
+        System.out.println("shoop = " + shoop);
     }
 
-    private static void deleteProduct(BufferedReader reader) throws IOException {
-        System.out.println("Delete by product id");
-        String id = reader.readLine();
-        DbStorage.deleteProduct(id);
+    private static void updateProduct(BufferedReader reader) throws IOException {
+        System.out.println("Let's update product's information. To start, please enter product's ID:");
+        String productId = reader.readLine();
+        Product product = DbStorage.getProduct(productId);
+        if (product == null) {
+            System.out.println("This product does not exist.");
+        } else {
+            System.out.println("Please enter product's new name:");
+            String productName = reader.readLine();
+            product.setProductName(productName);
+            System.out.println("Please enter product's price:");
+            String price = reader.readLine();
+            product.setPrice(price);
+            System.out.println("Please enter product's country:");
+            String country = reader.readLine();
+            product.setCountry(country);
+            System.out.println("Thank you, the product's information is updated.");
+        }
     }
-        private static void deleteShoop(BufferedReader reader) throws IOException {
+
+    private static void updateShoop(BufferedReader reader) throws IOException {
+        System.out.println("Let's update shoop's information. To start, please enter shoop's ID:");
+        String shoopId = reader.readLine();
+        Shoop shoop = DbStorage.getShoop(shoopId);
+        if (shoop == null) {
+            System.out.println("This shoop does not exist.");
+        } else {
+            System.out.println("Please enter shoop's name:");
+            String shoopName = reader.readLine();
+            shoop.setShoopName(shoopName);
+            System.out.println("Please enter product's country:");
+            String country = reader.readLine();
+            shoop.setCountry(country);
+            System.out.println("Thank you, the shoop's information is updated.");
+        }
+    }
+
+    private static void attachShoopToProduct(BufferedReader reader) throws IOException {
+        System.out.println("Let's attach the product to shoop. To start, please enter shoop's ID:");
+        String productId = reader.readLine();
+        System.out.println("Then please enter product's ID:");
+        String shoopId = reader.readLine();
+        if (productId == null || shoopId == null) {
+            System.out.println("This ID does not exist.");
+        } else {
+            DbStorage.attach(productId, shoopId);
+            System.out.println("Attachment is successful.");
+        }
+    }
+
+    private static void attachProductToShoop(BufferedReader reader) throws IOException {
+        System.out.println("Let's attach the shoop to product. To start, please enter product's ID:");
+        String shoopId = reader.readLine();
+        System.out.println("Then please enter shoop's ID:");
+        String productId = reader.readLine();
+        if (productId == null || shoopId == null) {
+            System.out.println("This ID does not exist.");
+        } else {
+            DbStorage.attach(productId, shoopId);
+            System.out.println("Attachment is successful.");
+        }
+
+    }
+        private static void deleteProductToShoop (BufferedReader reader) throws IOException {
+            System.out.println("Delete product to shoop");
+            System.out.println("Please enter shoop id");
+            String shoopId = reader.readLine();
+            System.out.println("Please enter product id");
+            String productId = reader.readLine();
+            DbStorage.deleteProductToShoop(productId, shoopId);
+        }
+
+        private static void deleteProduct (BufferedReader reader) throws IOException {
+            System.out.println("Delete by product id");
+            String id = reader.readLine();
+            DbStorage.deleteProduct(id);
+        }
+        private static void deleteShoop (BufferedReader reader) throws IOException {
             System.out.println("Delete by shoop id");
             String id = reader.readLine();
             DbStorage.deleteShoop(id);
         }
 
 
-        private static void findAllProduct() {
+        private static void findAllProduct () {
             System.out.println("Find all product");
             Product[] products = DbStorage.getProduct();
             System.out.println(Arrays.toString(products));
         }
-        private static void findAllShoop() {
+        private static void findAllShoop () {
             System.out.println("Find all shoop");
             Shoop[] shoops = DbStorage.getShoop();
             System.out.println(Arrays.toString(shoops));
 
         }
 
-        private static void exit() {
+        private static void exit () {
             System.exit(0);
 
         }
--- unit_5_module_1/src/main/java/ua/com/alevel/db/DbStorage.java
+++ unit_5_module_1/src/main/java/ua/com/alevel/db/DbStorage.java
@@ -19,40 +19,41 @@ public class DbStorage {
     public static void addProduct(Product product) {
         product.setId(generateProductId());
 
-       for (int i = 0; i< products.length; i++) {
+        for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
                 products[i] = product;
-            break;
+                break;
 
             }
-            if (products[products.length - 1] != null  ) {
+            if (products[products.length - 1] != null) {
                 productsArray = new Product[products.length * 2];
                 for (int j = 0; j < products.length; j++) {
                     productsArray[j] = products[j];
                 }
                 products = productsArray;
-                }
             }
         }
+    }
 
-     public static void addShoop(Shoop shoop) {
+    public static void addShoop(Shoop shoop) {
         shoop.setId(generateShoopId());
 
-         for (int i = 0; i< shoops.length; i++) {
-             if (shoops[i] == null) {
-                 shoops[i] = shoop;
-                 break;
+        for (int i = 0; i < shoops.length; i++) {
+            if (shoops[i] == null) {
+                shoops[i] = shoop;
+                break;
 
-             }
-             if (shoops[shoops.length - 1] != null  ) {
-                 shoopsArray = new Shoop[shoops.length * 2];
-                 for (int j = 0; j < shoops.length; j++) {
-                     shoopsArray[j] = shoops[j];
-                 }
-                 shoops = shoopsArray;
-             }
-         }
-     }
+            }
+            if (shoops[shoops.length - 1] != null) {
+                shoopsArray = new Shoop[shoops.length * 2];
+                for (int j = 0; j < shoops.length; j++) {
+                    shoopsArray[j] = shoops[j];
+                }
+                shoops = shoopsArray;
+            }
+        }
+    }
+
     private static String generateProductId() {
         String id = UUID.randomUUID().toString();
 
@@ -61,7 +62,7 @@ public class DbStorage {
 
                 if (products[i].getId().equals(id)) {
                     return generateProductId();
-        }
+                }
             }
         }
         return id;
@@ -71,9 +72,9 @@ public class DbStorage {
         String id = UUID.randomUUID().toString();
         for (int i = 0; i < shoops.length; i++) {
             if (shoops[i] != null) {
-            if (shoops[i].getId().equals(id)) {
-                return generateShoopId();
-            }
+                if (shoops[i].getId().equals(id)) {
+                    return generateShoopId();
+                }
             }
         }
         return id;
@@ -85,58 +86,63 @@ public class DbStorage {
                 if (products[i].getId().equals(id)) {
                     products[i] = null;
                     return;
-}
+                }
 
             }
         }
         System.out.println("Wrong id");
     }
+
     public static void deleteShoop(String id) {
         for (int i = 0; i < shoops.length; i++) {
             if (shoops[i] != null) {
                 if (shoops[i].getId().equals(id)) {
                     shoops[i] = null;
                     return;
-}
+                }
 
             }
         }
         System.out.println("Wrong id");
     }
-public static Product getProduct(String id) {
-    for (int i = 0; i < products.length; i++) {
-        if (products[i] != null) {
-            if (products[i].getId().equals(id)) {
-                return products[i];
+
+    public static Product getProduct(String id) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] != null) {
+                if (products[i].getId().equals(id)) {
+                    return products[i];
+                }
             }
         }
+        return null;
     }
-    return null;
-}
 
     public static void attach(String productId, String shoopId) {
-
-        attachProductToShoop(productId, shoopId);
+        attachShoopToProduct(productId, shoopId);
         attachProductToShoop(productId, shoopId);
     }
 
-    private static void attachProductToShoop(String productId, String shoopId) {
+    private static void attachShoopToProduct(String shoopId, String productId) {
         Shoop shoop = getShoop(shoopId);
-        String products = shoop.getId();
-        products = addProductToShoop(new String[]{products}, productId);
-        shoop.setId(Arrays.toString(products.toCharArray()));
-        Product product = getProduct(productId);
-        String shoops = product.getId();
-        shoops = addProductToShoop(new String[]{shoops}, shoopId);
-        product.setId(Arrays.toString(shoops.toCharArray()));
+        String[] products = shoop.getShoopIdList();
+        products = addUniqueShoopToProduct(products, productId);
+        shoop.setShoopIdList(products);
     }
 
-    private static String addProductToShoop(String[] array, String element) {
+
+    private static void attachProductToShoop(String shoopId, String productId) {
+        Product product = getProduct(productId);
+        String[] shoops = product.getProductIdList();
+        shoops = addUniqueShoopToProduct(shoops, productId);
+        product.setProductIdList(shoops);
+    }
+
+    private static String[] addUniqueShoopToProduct(String[] array, String element) {
         int emptyIndex = -1;
         for (int i = 0; i < array.length; i++) {
             if (array[i] != null) {
                 if (array[i].equals(element)) {
-                    return Arrays.toString(array);
+                    return array;
                 }
             } else if(emptyIndex == -1){
                 emptyIndex = i;
@@ -144,13 +150,15 @@ public static Product getProduct(String id) {
         }
 
         if(emptyIndex == -1) {
-            String[] temp = Arrays.copyOf(array, array.length + 5);
+            String[] temp = Arrays.copyOf(array, array.length + 0);
             array = temp;
         }
         array[emptyIndex] = element;
-        return Arrays.toString(array);
+        return array;
     }
-public static Shoop getShoop(String id) {
+
+
+    public static Shoop getShoop(String id) {
                     for (int i = 0; i < shoops.length; i++) {
                         if (shoops[i] != null) {
                             if (shoops[i].getId().equals(id)) {
@@ -186,7 +194,6 @@ public static Shoop getShoop(String id) {
                     }
                     System.out.println("Wrong id");
                 }
-            }}}
-    public static void addProductToShoop(String shoopId, String productId) {
+            }}
     }
 }
