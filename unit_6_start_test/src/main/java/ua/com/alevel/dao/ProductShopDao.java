8a72db9df48f4bd811d26e5ca9541d93c1306843
egorfuntusov <funtushan@gmail.com>
2023-01-28 17:21:37 +0200

    add feedback

--- unit_6_start_test/src/main/java/ua/com/alevel/dao/ProductShopDao.java
+++ unit_6_start_test/src/main/java/ua/com/alevel/dao/ProductShopDao.java
@@ -1,6 +1,5 @@
 package ua.com.alevel.dao;
 
-import ua.com.alevel.db.DbStorage;
 import ua.com.alevel.entity.Shop;
 import ua.com.alevel.entity.Product;
 
@@ -8,24 +7,15 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductShopDao {
+
     void createShop(Shop shop);
-
     void createProduct(Product product);
-
     void updateShop(Shop shop);
-
     void updateProduct(Product product);
-
     void deleteShop(String id);
-
     void deleteProduct(String id);
-
     List<Shop> findAllShop();
-
     List<Product> findAllProduct();
-
     Optional<Shop> getShop(String id);
-
     Optional<Product> getProduct(String id);
-
 }
--- unit_6_start_test/src/main/java/ua/com/alevel/service/ProductShopService.java
+++ unit_6_start_test/src/main/java/ua/com/alevel/service/ProductShopService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class ProductShopService {
+
     private ProductShopDao productShopDao = new ProductShopDao1();
 
     public void createShop(Shop shop) {
@@ -23,33 +24,25 @@ public class ProductShopService {
         return productShopDao.findAllShop();
     }
 
-
     public List<Product> findAllProduct() {
         return productShopDao.findAllProduct();
     }
 
-
     public Optional<Shop> getShop(String id) {
         return productShopDao.getShop(id);
-
     }
-
 
     public Optional<Product> getProduct(String id) {
         return productShopDao.getProduct(id);
-
-
     }
 
     public void deleteShop(String id) {
         productShopDao.deleteShop(id);
     }
 
-
     public void deleteProduct(String id) {
         productShopDao.deleteProduct(id);
     }
-
 
     public void updateShop(Shop shop) {
         productShopDao.updateShop(shop);
